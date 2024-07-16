71f1a81df55306649ab1b2aef4857e9d44df88bb
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2024-07-15 22:12:31 -0300

    start

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/go-away-learning/generics
+
+go 1.22.2
---
+++ main_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSumInts(t *testing.T) {
+	want := int64(4)
+	numbers := map[string]int64{
+		"first":  2,
+		"second": 2,
+	}
+	got := SumInts(numbers)
+
+	if got != want {
+		t.Errorf("got %d want %d, given %v", got, want, numbers)
+	}
+}
+
+func TestSumFloats(t *testing.T) {
+	want := 3.50
+	numbers := map[string]float64{
+		"first":  1.75,
+		"second": 1.75,
+	}
+	got := SumFloats(numbers)
+
+	if got != want {
+		t.Errorf("got %f want %f, given %v", got, want, numbers)
+	}
+}
